0ceb712266f18a6751084d6fe3907d5f5710470e
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-02-21 16:58:43 -0300

    feat: club findone aggregation

--- src/repositories/clubsRepository.ts
+++ src/repositories/clubsRepository.ts
@@ -1,9 +1,29 @@
+import { ObjectId } from "mongodb";
 import { Club } from "../models/Clubs";
 import { MongoRepository } from "./base/mongoRepository";
 
 export class ClubsRepository extends MongoRepository<Club> {
   public override async find(item: Partial<Club> = {}): Promise<Club[]> {
-    return await this.collection
-      .aggregate([{$match: item}]).lookup({from: "players", localField: "_id", foreignField: "club", as: "players"})
+    return await this.collection.aggregate([{ $match: item }]).lookup({
+      from: "players",
+      localField: "_id",
+      foreignField: "club",
+      as: "players",
+    });
+  }
+
+  public override async findOne(
+    item: string 
+  ): Promise<Club | null> {
+    const [club] = await this.collection
+      .aggregate([{ $match: { _id: new ObjectId(item)}}])
+      .lookup({
+        from: "players",
+        localField: "_id",
+        foreignField: "club",
+        as: "players",
+      })
+
+    return club
   }
 }
